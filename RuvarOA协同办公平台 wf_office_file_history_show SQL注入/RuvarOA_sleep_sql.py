c58832835883b5d08af90f46fd4ef131be3274dc
Grilled Cold Noodles <3084418174@qq.com>
2024-07-01 08:25:17 +0800

    Update RuvarOA_sleep_sql.py

--- RuvarOA协同办公平台 wf_office_file_history_show SQL注入/RuvarOA_sleep_sql.py
+++ RuvarOA协同办公平台 wf_office_file_history_show SQL注入/RuvarOA_sleep_sql.py
@@ -5,17 +5,7 @@ from multiprocessing.dummy import Pool
 requests.packages.urllib3.disable_warnings()
 
 def banner():
-    test = """
- 
-  ____   ___  _       _        _           _   _             
- / ___| / _ \| |     (_)_ __  (_) ___  ___| |_(_) ___  _ __  
- \___ \| | | | |     | | '_ \ | |/ _ \/ __| __| |/ _ \| '_ \ 
-  ___) | |_| | |___  | | | | || |  __/ (__| |_| | (_) | | | |
- |____/ \__\_\_____| |_|_| |_|/ |\___|\___|\__|_|\___/|_| |_|
-                            |__/                             
-                                                                                                      
-"""
-    print(test)
+    pass
 
 # 主函数模块
 def main():
